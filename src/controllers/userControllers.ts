db5464ac384728f40bb32cd80c70b1c9f9cbb910
Junos16 <hriddhitdatta2002@gmail.com>
2024-04-07 09:54:30 +0530

    Add middleware for active user, fix issues, remove redundancies

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -3,14 +3,14 @@ import { UserService } from "../services/userService";
 
 const userService = new UserService();
 
-export const createUser = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const newUser = await userService.createUser(req.body);
-        res.status(201).json(newUser)
-    } catch(error) {
-        res.status(500).json({ message: error.message });
-    }
-};
+// export const createUser = async (req: Request, res: Response): Promise<void> => {
+//     try {
+//         const newUser = await userService.createUser(req.body);
+//         res.status(201).json(newUser)
+//     } catch(error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// };
 
 export const getUserByID = async (req: Request, res: Response): Promise<void> => {
     try {
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,14 +1,18 @@
 import express from "express";
-import { createUser, deleteUser, getUserByID, updateUser } from "../controllers/userControllers";
+import { deleteUser, getUserByID, updateUser } from "../controllers/userControllers";
 import { requireAuth } from "../middleware/requireAuth";
 import { requireRole } from "../middleware/requireRole";
 import { UserRole } from "../../types/userRole";
+import { requireUser } from "../middleware/requireUser";
 
 const userRouter = express.Router();
 
-userRouter.post("/", requireAuth, requireRole(UserRole.ADMIN), createUser);
+// userRouter.post("/", requireAuth, requireRole(UserRole.ADMIN), createUser);
 userRouter.get("/:id", requireAuth, requireRole(UserRole.ADMIN), getUserByID);
+userRouter.get("/:id", requireAuth, requireUser, getUserByID);
 userRouter.put(":/id", requireAuth, requireRole(UserRole.ADMIN), updateUser);
+userRouter.put(":/id", requireAuth, requireUser, updateUser);
 userRouter.delete("/:id", requireAuth, requireRole(UserRole.ADMIN), deleteUser);
+userRouter.delete("/:id", requireAuth, requireUser, deleteUser);
 
 export default userRouter;
